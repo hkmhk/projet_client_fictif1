91e0f8824bf97286f29539d2ebdbd339c6da0f2e
tifus69 <tifus69@hotmail.com>
2018-03-15 19:53:43 +0100

    Added parallax effect Added  map and contact form in infos section Added footer section

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+  // initialize paroller.js and set attributes
+  $("#entete").paroller({
+    factor: "0.5",
+    type: "background"
+    // coverRatio: 1
+    // direction: "vertical"
+  });
+});
