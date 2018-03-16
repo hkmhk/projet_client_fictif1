454bc52f2651a8ce4252bdd1ac9f8c47c318532d
BJDsrchs <benjamin.desroches@gmail.com>
2018-03-16 10:53:12 +0100

    fadein about

--- essai.js
+++ essai.js
@@ -1,12 +1,23 @@
-$(document).ready(function(){
-    $(".flippy").flip({
-        axis: 'y',
-        trigger: 'click'
-      });
+$(document).ready(function() {
+  $(".flippy").flip({
+    axis: "y",
+    trigger: "click"
+  });
 
-    $('.navbutton__link').click(function(){
-        document.querySelector(".navbutton__checkbox").checked = false;
-    });
-      
-    $('#grandLogo, #reserverBtn, #entete').addClass('animated fadeIn');
+  $(".navbutton__link").click(function() {
+    document.querySelector(".navbutton__checkbox").checked = false;
+  });
+
+  let animHome = $("#grandLogo, #reserverBtn, #entete").addClass(
+    "animated fadeIn"
+  );
+
+  let animAboutR = $("#animRight").addClass("animated FadeInLeft");
+  let animAboutL = $("#animLeft").addClass("animated FadeInRight");
+
+  //   var waypoint = new Waypoint({
+  //     element: document.getElementById('animRight'),
+  //     handler: function() {
+  //              let animAboutR = $("#animRight").addClass("animated slideInLeft")}
+  //   });
 });
